8b858d9671e569dcf91120dee72a5baf57bb7dc0
AndrewKushnir <cuckop30@gmail.com>
2022-04-01 15:27:26 +0300

    refactor WindowManager

--- src/main/java/Renderer/Renderer.java
+++ src/main/java/Renderer/Renderer.java
@@ -14,5 +14,6 @@ public class Renderer {
         glDrawElements(GL_TRIANGLES,model.getVertexCount(),GL_UNSIGNED_INT,0);
         glDisableVertexAttribArray(0);
         glBindVertexArray(0);//конец метода
+        //df/fd
     }
 }
