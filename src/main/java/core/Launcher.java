7c31b2ea1fe3f67e0ab1bd5e95156021b5e38c37
kirsanovdi <kirsanov.di@edu.spbstu.ru>
2022-04-15 14:19:37 +0300

    add tests, change previous test, launcher rework, correct textures in picture3

--- src/main/java/core/Launcher.java
+++ src/main/java/core/Launcher.java
@@ -12,7 +12,8 @@ import java.util.Random;
 
 
 public class Launcher {
-    GraphicsDisplay graphicsDisplay;
+    private final GraphicsDisplay graphicsDisplay;
+    private long delay = -1L;
     public MBO object;
     public SimulationSimple simulate = new SimulationSimple(2.0f, object);
     public MBO[] mbos;
@@ -20,58 +21,58 @@ public class Launcher {
     Launcher() {
         mbos = new MBO[]{
                 object,
-                //new MBO(genVoxelArray(10), new Vector3D(0.0f, 0.0f, 0.0f), 0.0f, 0.0f, 0.0f),
-                //new MBO(new Voxel[]{ new Voxel(0, 0, 0), new Voxel(0, 0, 1)} , new Vector3D(1.0f, 0.0f, 0.0f), 0.0f, 0.0f, 0.0f),
-                new MBO(MBO.genVoxelArray(50000), new Vector3D(0.0f, 1.0f, 0.0f), 0.0f, 0.0f, 0.0f),
-                //new MBO(genVoxelArray(1), new Vector3D(0.0f, 0.0f, -1.0f), 0.0f, 0.0f, 0.0f)
+                new MBO(MBO.genVoxelArray(10000), new Vector3D(0.0f, 1.0f, 0.0f), 0.0f, 0.0f, 0.0f),
         };
         object = new MBO(MBO.genVoxelArray(2), new Vector3D(0.0f, 0.0f, -10.0f), 0.0f, 0.0f, 0.0f);
         graphicsDisplay = new GraphicsDisplay(1080, 1080, "VoxelGame", this);
+        delay = 1000l;
     }
 
-    public Launcher(MBO[] mbos, MBO object) {
+    public Launcher(MBO[] mbos, MBO object, long delay) {
         this.mbos = mbos;
         this.object = object;
         this.graphicsDisplay = new GraphicsDisplay(1080, 1080, "VoxelGame", this);
+        this.delay = delay;
     }
 
-    public void run() {
+    public Launcher(MBO[] mbos) {
+        this.mbos = mbos;
+        this.graphicsDisplay = new GraphicsDisplay(1080, 1080, "VoxelGame", this);
+    }
+
+    public boolean toClose(){
+        return graphicsDisplay.toClose;
+    }
+
+    public void run(){
+        runSimulation();
+        runGUI();
+    }
+
+    public void runGUI(){
+        System.out.println("start gui");
+        graphicsDisplay.run();
+        System.out.println("close gui");
+    }
+
+    public void runSimulation() {
+        if(object == null) throw new IllegalStateException("object is null");
+        if(delay < 1) throw new IllegalStateException("incorrect delay");
         Thread thread = new Thread(() -> {
             System.out.println("Start runSim");
             while (!graphicsDisplay.toClose) {
                 simulate.setObject(object);
                 simulate.operate();
                 object.setVector3D(simulate.getObject());
-                //System.out.println(object.getX() + " " + object.getY() + " " + object.getZ());
                 try {
-                    Thread.sleep(1000L);
-                    synchronized (this) {
-                        /*if(Math.random() > 0.90) {
-                            mbos = new MBO[(int) (Math.random() * 200) + 6000 + 1];
-                        }
-                        mbos[0] = object;
-                        for (int i = 1; i < mbos.length; i++) {
-                            if(Math.random() > 0.99 || mbos[i] == null) {
-                                mbos[i] = new MBO(genVoxelArray(5), new Vector3D((int) (Math.random() * 640), (int) (Math.random() * 640), (int) (Math.random() * 640)), 0.0f, 0.0f, 0.0f);
-                            }
-                        }
-                        for (int j = 0; j < mbos.length; j++) {
-                            if (mbos[j] == null) {
-                                System.out.println(1);
-                            }
-                        }*/
-                    }
-                } catch (Exception e) {
-                    System.out.println(e);
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                //System.out.println(graphicsDisplay.toClose);
             }
             System.out.println("End runSim");
         });
         thread.start();
-        System.out.println("start gui");
-        graphicsDisplay.run();
-        System.out.println("close gui");
     }
 
 }
